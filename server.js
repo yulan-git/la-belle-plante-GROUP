dfabd8387c2908289686e8233efaa376d039bb39
gaelle1701 <gaellecoue@gmail.com>
2022-03-01 15:43:37 +0100

    test run server 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/la-belle-plante'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/la-belle-plante/index.html') );});
